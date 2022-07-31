e4ed2b8823445c28f80d970b3f8f4b54e8087dca
Cocoa <r.xu.1@research.gla.ac.uk>
2022-07-31 17:50:43 +0100

    init

---
+++ lib/cc_precompiler_example.ex
@@ -0,0 +1,18 @@
+defmodule CcPrecompilerExample do
+  @moduledoc """
+  Documentation for `CcPrecompilerExample`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> CcPrecompilerExample.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/cc_precompiler_example_test.exs
@@ -0,0 +1,8 @@
+defmodule CcPrecompilerExampleTest do
+  use ExUnit.Case
+  doctest CcPrecompilerExample
+
+  test "greets the world" do
+    assert CcPrecompilerExample.hello() == :world
+  end
+end
