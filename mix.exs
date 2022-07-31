2268e2073aa5d9529fab3bb697df592529612c6a
Cocoa <r.xu.1@research.gla.ac.uk>
2022-07-31 21:07:39 +0100

    updated deps

--- mix.exs
+++ mix.exs
@@ -10,7 +10,7 @@ defmodule CCPrecompilerExample.MixProject do
       compilers: [:elixir_make] ++ Mix.compilers(),
       make_nif_filename: "nif",
       make_precompiler: CCPrecompiler,
-      cc_precompile_base_url: "https://github.com/cocoa-xu/cc_precompiler_example/downloads/releases/v#{@version}",
+      cc_precompile_base_url: "https://github.com/cocoa-xu/cc_precompiler_example/releases/download/v#{@version}",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
@@ -24,6 +24,7 @@ defmodule CCPrecompilerExample.MixProject do
 
   defp deps do
     [
+      # todo: change this to hex.pm once elixir-lang/elixir_make#55 is merged
       {:elixir_make, "~> 0.6", runtime: false, github: "cocoa-xu/elixir_make", branch: "cx-fennec_precompile"},
       {:cc_precompiler, "~> 0.1.0", runtime: false, github: "cocoa-xu/cc_precompiler"}
     ]
