07f04385e54e5bced4b04c67bf3fc0b925d3bd9e
Cocoa <r.xu.1@research.gla.ac.uk>
2022-11-05 13:08:41 +0000

    update deps

--- mix.exs
+++ mix.exs
@@ -34,12 +34,9 @@ defmodule CCPrecompilerExample.MixProject do
   defp deps do
     [
       {:castore, "~> 0.1"},
-      {:elixir_make, "~> 0.6", runtime: false,
-        github: "cocoa-xu/elixir_make", branch: "cx-improve-precompiler"},
-        # path: "../elixir_make", override: true},
-      {:cc_precompiler, "~> 0.1.0", runtime: false,
-        github: "cocoa-xu/cc_precompiler"}
-        # path: "../cc_precompiler"}
+      {:elixir_make, "~> 0.6", runtime: false, github: "cocoa-xu/elixir_make", branch: "cx-improve-precompiler"},
+      {:stb_image, "~> 0.5", github: "cocoa-xu/stb_image"},
+      {:cc_precompiler, "~> 0.1.0", runtime: false, github: "cocoa-xu/cc_precompiler"}
     ]
   end
 
